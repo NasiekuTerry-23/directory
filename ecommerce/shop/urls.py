0e5097389857a20343d775869f143123555de4ce
featuri <estherperis2005@gmail.com>
2024-05-09 08:08:53 +0300

    my project

---
+++ ecommerce/shop/urls.py
@@ -0,0 +1,10 @@
+from shop import views
+from . import views
+
+from django.urls import path
+
+urlpatterns = [
+
+    path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+]
